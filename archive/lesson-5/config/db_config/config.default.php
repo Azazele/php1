eaa3a62dbb9b6e7ec621e07bd932d9ffa3314344
david_makhnev <devidwm97@gmail.com>
2019-08-22 13:58:42 +0300

    Подготовка пространства для ДЗ 6

---
+++ archive/lesson-5/config/db_config/config.default.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'db_host' => 'localhost:3306',
+	'db_user' => 'root',
+	'db_password' => 'Greench97',
+	'db_name' => 'shop'
+];
--- archive/lesson-5/index.php
+++ archive/lesson-5/index.php
@@ -6,5 +6,12 @@ autoload('config');
 
 include TAMPLATES_DIR.'header.php';
 
+$page = $_GET['id'];
+if ($page){
+	include TAMPLATES_DIR.'big_img.php';
+} else {
+	include ENGINE_DIR.'lesson5.php';
+	include PUBLIC_DIR.'lesson5.php';
+}
 
 include TAMPLATES_DIR.'footer.php';
